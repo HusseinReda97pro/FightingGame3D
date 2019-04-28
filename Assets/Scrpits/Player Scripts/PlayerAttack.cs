41bdfd870d7b6113fc05c2bf1f34c586bb1b0fc5
HusseinReda97pro <47584580+HusseinReda97pro@users.noreply.github.com>
2019-04-28 16:27:42 +0200

    First 1.5 Hour

--- Assets/Scrpits/Player Scripts/PlayerAttack.cs
+++ Assets/Scrpits/Player Scripts/PlayerAttack.cs
@@ -2,9 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ComboState{
+NONE,
+    PUNCH_1,
+    PUNCH_2,
+    PUNCH_3,
+    KICK_1,
+    KICK_2
+}
+
 public class PlayerAttack : MonoBehaviour
 {
     private CharacterAnimation player_Anim;
+    private bool activateTimerToReset;
+
+    private float default_Combo_Timer = 0.4f;
+    private float current_Combo_Timer;
+
+    private ComboState current_Combo_State;
 
 
     void Awake()
@@ -17,13 +32,17 @@ public class PlayerAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        current_Combo_Timer = default_Combo_Timer;
+        current_Combo_State = ComboState.NONE;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         ComoAttacks();
+        ResetComboState();
 
 
     }
@@ -31,15 +50,70 @@ public class PlayerAttack : MonoBehaviour
     void ComoAttacks() {
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            player_Anim.Punch_1(); 
+            if (current_Combo_State == ComboState.PUNCH_3 ||
+               current_Combo_State == ComboState.KICK_1 ||
+               current_Combo_State == ComboState.KICK_2)
+                return;
+
+                current_Combo_State++;
+            activateTimerToReset = true;
+            current_Combo_Timer = default_Combo_Timer;
+            if (current_Combo_State == ComboState.PUNCH_1) {
+                player_Anim.Punch_1();
+
+            }
+            if (current_Combo_State == ComboState.PUNCH_2)
+            {
+                player_Anim.Punch_2();
+
+            }
+            if (current_Combo_State == ComboState.PUNCH_3)
+            {
+                player_Anim.Punch_3();
+
+            }
+
+
         }
         if (Input.GetKeyDown(KeyCode.X))
         {
-            player_Anim.Punch_2();
+            if (current_Combo_State == ComboState.PUNCH_3 ||
+                current_Combo_State == ComboState.KICK_2)
+                return;
+            if (current_Combo_State == ComboState.NONE ||
+                current_Combo_State == ComboState.PUNCH_1 ||
+                current_Combo_State == ComboState.PUNCH_2)
+            {
+                current_Combo_State = ComboState.KICK_1;
+            }else if(current_Combo_State == ComboState.KICK_1)
+            {
+                current_Combo_State++;
+            }
+            activateTimerToReset = true;
+            current_Combo_Timer = default_Combo_Timer;
+            if (current_Combo_State == ComboState.KICK_1)
+            { // move to kick 2
+                player_Anim.Kcik_1();
+
+            }
+            if (current_Combo_State == ComboState.KICK_2)
+            {
+                player_Anim.Kcik_2();
+
+            }
         }
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            player_Anim.Punch_3();
+        
+    }
+
+    void ResetComboState() {
+        if (activateTimerToReset) {
+            current_Combo_Timer -= Time.deltaTime;
+            if (current_Combo_Timer <= 0f) {
+                current_Combo_State = ComboState.NONE;
+                activateTimerToReset = false;
+                current_Combo_Timer = default_Combo_Timer;
+
+            }
         }
     }
 }
