8d96c756d462084275ee6ef4c74c1fe9dfd7cccd
HusseinReda97pro <47584580+HusseinReda97pro@users.noreply.github.com>
2019-04-28 15:15:15 +0200

    Create Project

---
+++ Assets/Scrpits/Player Scripts/CharacterAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    public void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void Walk(bool move)
+    {
+        anim.SetBool(AnimationTags.MOVEMENT, move);
+    }
+    public void Punch_1()
+    {
+        anim.SetTrigger(AnimationTags.PUNCH_1_TRIGGER);
+    }
+    public void Punch_2()
+    {
+        anim.SetTrigger(AnimationTags.PUNCH_2_TRIGGER);
+    }
+    public void Punch_3()
+    {
+        anim.SetTrigger(AnimationTags.PUNCH_3_TRIGGER);
+    }
+    public void Kcik_1()
+    {
+        anim.SetTrigger(AnimationTags.KICK_1_TRIGGER);
+    }
+    public void Kcik_2()
+    {
+        anim.SetTrigger(AnimationTags.KICK_2_TRIGGER);
+    }
+
+
+}
---
+++ Assets/Scrpits/Player Scripts/PlayerAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private CharacterAnimation player_Anim;
+
+
+    void Awake()
+    {
+        player_Anim = GetComponentInChildren<CharacterAnimation>();
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ComoAttacks();
+
+
+    }
+
+    void ComoAttacks() {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            player_Anim.Punch_1(); 
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            player_Anim.Punch_2();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            player_Anim.Punch_3();
+        }
+    }
+}
